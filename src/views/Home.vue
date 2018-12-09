4e8cce912b533fb635543fd5da1ae8295e01041e
FlorianWoelki <FlorianWoelki@gmx.de>
2018-12-09 20:14:20 +0100

    Added a button for going to the next page and added max date

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,22 @@
   <div class="home text-xs-center">
     <h1 class="subheading mb-4">Select your bithday!</h1>
 
-    <v-date-picker
-      v-model="picker"
-      :landscape="true"
-      :reactive="true"
-    ></v-date-picker>
+    <v-flex xs12>
+      <v-date-picker
+        v-model="picker"
+        max="2019"
+        :landscape="true"
+        :reactive="true"
+      ></v-date-picker>
+    </v-flex>
+    <v-flex xs12>
+      <v-btn
+        large
+        outline
+        color="info"
+        class="mb-4 my-4"
+      >Continue</v-btn>
+    </v-flex>
   </div>
 </template>
 
