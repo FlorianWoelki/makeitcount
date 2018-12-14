343ce112d15892dd8d805f09664092a3d4bb01e8
FlorianWoelki <FlorianWoelki@gmx.de>
2018-12-14 18:25:22 +0100

    Updated grid for calendar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,12 +3,24 @@
     <h1 class="subheading mb-4">Welcome to the Dashboard!</h1>
     <p>{{ this.pickedDate }}</p>
 
-    <div class="weeks">
-      <span
+    <ul class="weeks-title">
+      <li></li>
+      <li
         v-for="i in 52"
-        :key=i
-      >{{i}} </span>
-    </div>
+        :key="'weeks-title-' + i"
+      >{{i}}</li>
+    </ul>
+    <ul
+      class="weeks-boxes"
+      v-for="i in 81"
+      :key="'weeks-boxes-' + i"
+    >
+      <li>{{i - 1}}</li>
+      <li
+        v-for="j in 52"
+        :key="'weeks-boxes-line-' + j"
+      >{{j}}</li>
+    </ul>
   </div>
 </template>
 
@@ -25,9 +37,34 @@ export default {
 </script>
 
 <style>
-.weeks span {
+.weeks-title {
+  font-size: 10px;
+  color: #777;
+}
+.weeks-title li {
+  margin: 1px;
+  display: inline-block;
+  width: 1.6%;
+  color: #666;
+  text-align: center;
+}
+
+.weeks-boxes {
   font-size: 12px;
-  font-weight: 300;
+  color: #777;
+}
+.weeks-boxes li:first-child {
+  border-style: none;
+}
+.weeks-boxes li {
+  margin: 1px;
+  list-style-type: none;
+  display: inline-block;
+  width: 1.6%;
+  border-style: solid;
+  border-width: 1px;
+  text-align: center;
+  margin-bottom: 1px;
 }
 </style>
 
