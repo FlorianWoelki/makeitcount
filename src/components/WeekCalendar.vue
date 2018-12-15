ef83547bbdac1d0f1569ab0a520dd3ee4244f804
FlorianWoelki <FlorianWoelki@gmx.de>
2018-12-15 14:04:58 +0100

    Refactored WeekCalendar to extra component

---
+++ src/components/WeekCalendar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="week-calendar">
+    <ul class="horizontal-lines">
+      <li></li>
+      <li
+        v-for="i in 52"
+        :key="'title-' + i"
+      >{{i}}</li>
+    </ul>
+    <ul
+      class="boxes"
+      v-for="i in 81"
+      :key="'boxes-' + i"
+    >
+      <li>{{i - 1}}</li>
+      <li
+        v-for="j in 52"
+        :key="'boxes-line-' + j"
+      >{{j}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeekCalendar'
+}
+</script>
+
+
+<style>
+.week-calendar .horizontal-lines {
+  font-size: 10px;
+  color: #777;
+}
+.week-calendar .horizontal-lines li {
+  margin: 1px;
+  display: inline-block;
+  width: 1.6%;
+  color: #666;
+  text-align: center;
+}
+
+.week-calendar .boxes {
+  font-size: 12px;
+  color: #777;
+}
+.week-calendar .boxes li:first-child {
+  border-style: none;
+}
+.week-calendar .boxes li {
+  margin: 1px;
+  list-style-type: none;
+  display: inline-block;
+  width: 1.6%;
+  border-style: solid;
+  border-width: 1px;
+  text-align: center;
+  margin-bottom: 1px;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,30 +3,16 @@
     <h1 class="subheading mb-4">Welcome to the Dashboard!</h1>
     <p>{{ this.pickedDate }}</p>
 
-    <ul class="weeks-title">
-      <li></li>
-      <li
-        v-for="i in 52"
-        :key="'weeks-title-' + i"
-      >{{i}}</li>
-    </ul>
-    <ul
-      class="weeks-boxes"
-      v-for="i in 81"
-      :key="'weeks-boxes-' + i"
-    >
-      <li>{{i - 1}}</li>
-      <li
-        v-for="j in 52"
-        :key="'weeks-boxes-line-' + j"
-      >{{j}}</li>
-    </ul>
+    <WeekCalendar />
   </div>
 </template>
 
 <script>
+import WeekCalendar from '@/components/WeekCalendar'
+
 export default {
   name: 'Dashboard',
+  components: { WeekCalendar },
   props: {
     pickedDate: {
       type: String,
@@ -35,36 +21,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.weeks-title {
-  font-size: 10px;
-  color: #777;
-}
-.weeks-title li {
-  margin: 1px;
-  display: inline-block;
-  width: 1.6%;
-  color: #666;
-  text-align: center;
-}
-
-.weeks-boxes {
-  font-size: 12px;
-  color: #777;
-}
-.weeks-boxes li:first-child {
-  border-style: none;
-}
-.weeks-boxes li {
-  margin: 1px;
-  list-style-type: none;
-  display: inline-block;
-  width: 1.6%;
-  border-style: solid;
-  border-width: 1px;
-  text-align: center;
-  margin-bottom: 1px;
-}
-</style>
-
